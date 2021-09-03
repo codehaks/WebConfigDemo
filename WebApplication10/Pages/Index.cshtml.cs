0cac783451cdf8a7d7dc37088b96cd4111a83b1e
Hakim Ghods <ghods.hakim@hotmail.com>
2021-09-03 10:45:59 +0430

    Add project files.

---
+++ WebApplication10/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace WebApplication10.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public List<KeyValuePair<string, string>> ConfigList { get; set; }
+
+        public string Message { get; set; }
+        public string Url { get; set; }
+        public int Year { get; set; }
+        public void OnGet()
+        {
+            Message = _configuration["Message"];
+
+            Url = _configuration["app:url"];
+            Year = int.Parse(_configuration["app:year"]);
+
+            var configs = _configuration.GetChildren();
+            ConfigList = new List<KeyValuePair<string, string>>();
+
+            foreach (var config in configs)
+            {
+                ConfigList.Add(new KeyValuePair<string, string>(config.Key, config.Value));
+            }
+        }
+    }
+}
